308da8bc4ad55a9697511d16a4037dde3c3e5071
Matt Keehan <matthew.keehan@experian.com>
2018-09-05 15:22:11 +0100

    added some simple assertions to prove functionality

--- colours/colours.js
+++ colours/colours.js
@@ -16,3 +16,9 @@ const colours = (col1, col2) => {
 
     return calculatedColourSet.join('');
 };
+
+//Console shows failed assertions only
+console.assert(colours('111111', '333333') === '222222', {"message": "test 1"});
+console.assert(colours('000000', '002200') === '001100', {"message": "test 2"});
+console.assert(colours('000000', '001100') === '000900', {"message": "test 3"});
+console.assert(colours('ff0000', 'ff0000') === 'ff0000', {"message": "test 4"});
--- html-arrays/html-arrays.js
+++ html-arrays/html-arrays.js
@@ -24,3 +24,10 @@ const getHtmlString = inputArray => {
 	return htmlStartString + htmlEndString;
 };
 
+//Console shows failed assertions only
+console.assert(getHtmlString(['div']) === '<div/>', {"message": "test 1"});
+console.assert(getHtmlString(['h1', 'Text']) === '<h1>Text</h1>', {"message": "test 2"});
+console.assert(getHtmlString(['span', 'More', 'Text']) === '<span>MoreText</span>', {"message": "test 3"});
+console.assert(getHtmlString(['a', ['b', 'More'], ' Text']) === '<a><b>More</b> Text</a>', {"message": "test 4"});
+console.assert(getHtmlString(['p', '<b>Text</b>']) === '<p><b>Text</b></p>', {"message": "test 5"});
+console.assert(getHtmlString(['a', ['b', 'More', ['i', 'one', ' two']], ' Text']) === '<a><b>More<i>one two</i></b> Text</a>', {"message": "test 6"});
